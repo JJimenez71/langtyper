273b452c52b3746c2b1eccb47ae8a5e2fc91f572
JJimenez71 <jordanjimenez510@gmail.com>
2024-04-18 18:50:31 -0600

    created basic typing exercise

---
+++ src/routes/TypingTest.svelte
@@ -0,0 +1,25 @@
+<script>
+    let passage = 'This is my first time trying this.';
+    let currInput = "";
+    function handleInput(event){
+        currInput = event.target.value;
+    }
+</script>
+
+<div>
+    <p>
+        {#each passage.split('') as char, index}
+            <span class={currInput.length > index && currInput[index] === char ? 'correct' : 'incorrect'}>{char}</span>
+        {/each}
+    </p>
+</div>
+<input type="text" bind:value={currInput} on:input={handleInput}/>
+
+<style>
+    .correct{
+        color: green;
+    }
+    .incorrect{
+        color:red;
+    }
+</style>
